9955c860b8203375efe35b246571d6e766c89f4b
Musadiq Khan <musadiq.khan@gmail.com>
2020-06-28 21:28:57 +0500

    Update brands.js

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -4,7 +4,47 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 
 const Brands = () => {
-  return <div></div>
+  const shoppers = useStaticQuery(graphql`
+    query {
+      allContentfulBrands {
+        edges {
+          node {
+            brandshero {
+              file {
+                url
+              }
+            }
+
+            brandsherotitle
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div>
+      <div className="header_div">
+        <Header />
+      </div>
+      <div
+        className="shoppers-hero"
+        style={{
+          backgroundImage: `url(${
+            shoppers.allContentfulBrands.edges[0].node.brandshero.file.url +
+            "?fm=jpg&fl=progressive"
+          })`,
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+          backgroundPosition: "center top",
+          backgroundSize: "cover",
+          objectFit: "cover",
+        }}
+      >
+        <h1>{shoppers.allContentfulBrands.edges[0].node.brandsherotitle}</h1>
+        <p>{shoppers.allContentfulShoppers.edges[0].node.merchantherotext}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Brands
